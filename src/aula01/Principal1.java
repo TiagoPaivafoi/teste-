a932f9c0787498f01ead3fd828ce645ddf55a23d
TiagoPaivafoi <tiagopaiva93@gmail.com>
2022-05-12 10:08:30 -0300

    primeiro comit

---
+++ src/aula01/Principal1.java
@@ -0,0 +1,47 @@
+package aula01;
+
+public class Principal1 {
+
+	public static void main(String[] args) {
+	
+		int idade;
+	    double salario;
+		String nome;
+		
+		nome = "Tiago";
+		salario = 20000.00;
+		idade = 18;
+		
+	
+		System.out.print( "nome:"+nome + " Idade:" +idade + " salario: R$"+salario );
+		
+		int x = 10;
+		int y = 2;	
+		
+		System.out.print(x + y);
+		
+		String comida = "Batata frita"; 
+		String sobreMesa = "Bolo" ;
+		System.out.print(comida);
+		System.out.print( x<<=y); 
+				
+		if(idade<18) {
+			System.out.print("Proibido dirigir");
+		}
+		else {
+			System.out.print("Pode dirigir");
+		}
+		
+		
+
+		
+		
+
+		
+			
+		
+
+	}
+
+}
+
